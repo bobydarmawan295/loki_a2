5ecdccf9ad3c730cfa544192c46eb82c975abd11
annisaulfa11 <2011522015_annisa@student.ac.id>
2022-03-17 23:18:08 +0700

    update

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const path = require("path");
 const app = express();
 const port = 8000;
 
+app.get("/profile", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Halooo");
+});
+
+app.get("/salam/:nama/:nim", function (req, res) {
+  res.send("Selamat Sore " + req.params.nama + " <br>nim anda " + req.params.nim);
+});
+
 app.listen(port, () => {
   console.log(`Server berada port  ${port}`);
 });
