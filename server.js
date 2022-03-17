9da4bde6564c6f7cce3dca5139a5ed577e00cb9a
RayhanRizaldi <91644727+RayhanRizaldi@users.noreply.github.com>
2022-03-17 23:10:09 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 8000
+const port = 8000;
 
 app.listen(port, () => {
     console.log(`Server berada port  ${port}`)
