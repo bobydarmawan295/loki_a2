47cfb8de4ed6d712e02cba5826b2ef1c713bf879
Dwisuci Insani Karimah <90795923+DwisuciInsani@users.noreply.github.com>
2022-03-18 00:40:32 +0700

    tambah package.json

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
+// import modul express
 const express = require("express");
 const path = require("path");
 const app = express();
 const port = 8000;
 
+//request halaman profil
 app.get("/profile", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Halooo");
 });
 
+//request halaman salam, nama, nim
 app.get("/salam/:nama/:nim", function (req, res) {
   res.send("Selamat Sore " + req.params.nama + " <br>nim anda " + req.params.nim);
 });
 
+//connect dengan port
 app.listen(port, () => {
   console.log(`Server berada port  ${port}`);
 });
