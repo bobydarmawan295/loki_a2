ff997ca6fcfa55a024131ba5c5f44af6f12bebce
Dwisuci Insani Karimah <90795923+DwisuciInsani@users.noreply.github.com>
2022-04-01 10:42:00 +0700

    Update course_plan_details

--- config/models/course_plan_details.js
+++ config/models/course_plan_details.js
@@ -1,1 +1,50 @@
-console.log("ini model")
+// skema orm course_plan_details
+const Sequelize = require('sequelize');
+const db= require('../database/conn');
+
+const cpDetail = db.define("cpDetail",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+      },
+
+      course_plan_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        foreignKey: true
+      },
+
+      week: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+
+      material: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      method: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+  
+      student_experience: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      update_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    },
+
+    {
+      tableName: "course_plan_lecturers",
+      timestamps: false,
+    }
+);
+module.exports = cpDetail;
