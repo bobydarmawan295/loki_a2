813698d7b828d827d63733d2a88ce34e927541ab
annisaulfa11 <91662109+annisaulfa11@users.noreply.github.com>
2022-04-01 09:53:38 +0700

    skema orm course_plans

--- config/models/course_plans.js
+++ config/models/course_plans.js
@@ -1,43 +1,43 @@
-const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize("mysql://root@localhost:8080/loki");
+// skema orm course_plans
+const Sequelize = require('sequelize');
+const db= require('../database/conn');
 
-module.exports = (sequelize, DataTypes) => {
-  const cp = sequelize.define(
+const cp = db.define(
     "course_plan",
     {
       id: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         autoIncrement: true,
         primaryKey: true,
       },
 
       course_id: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         autoIncrement: true,
         foreignKey: true,
       },
 
       rev: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         allowNull: false,
       },
 
       code: {
-        type: DataTypes.STRING,
+        type: Sequelize.STRING,
         allowNull: false,
       },
 
       name: {
-        type: DataTypes.STRING,
+        type: Sequelize.STRING,
         allowNull: false,
       },
 
       alias_name: {
-        type: DataTypes.STRING,
+        type: Sequelize.STRING,
       },
 
       credit: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         allowNull: false,
       },
 
@@ -47,40 +47,39 @@ module.exports = (sequelize, DataTypes) => {
       },
 
       description: {
-        type: DataTypes.STRING,
+        type: Sequelize.STRING,
       },
 
       material: {
-        type: DataTypes.STRING,
+        type: Sequelize.STRING,
       },
 
       created_by: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         autoIncrement: true,
         foreignKey: true,
       },
 
       validated_by: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         autoIncrement: true,
         foreignKey: true,
       },
 
       validated_at: {
-        type: DataTypes.DATE,
+        type: Sequelize.DATE,
       },
       created_at: {
-        type: DataTypes.DATE,
+        type: Sequelize.DATE,
       },
       update_at: {
-        type: DataTypes.DATE,
+        type: Sequelize.DATE,
       },
     },
 
     {
       tableName: "course_plan",
-      timestamps: true,
+      timestamps: false,
     }
   );
-  return cp;
-};
+module.exports = cp;
