141eec8c29e33b543de2e94cc063dbec3e715bf1
annisaulfa11 <2011522015_annisa@student.ac.id>
2022-04-22 23:31:14 +0700

    update

--- routes/lecturers.js
+++ routes/lecturers.js
@@ -5,11 +5,7 @@ const router = express.Router();
 const db= require('../config/database/conn');
 const controller = require('../controller/index');
 
-//router.use(express.json())
-
 router.get('/', controller.lecturers.getAll)
 
 module.exports = router;
-=======
 
->>>>>>> 6120a048f6448268bdb0c5765b9eca5783696d97
