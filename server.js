6d678cb7cd58cc418c864bcf47261025152e7f51
annisaulfa11 <2011522015_annisa@student.ac.id>
2022-04-29 20:05:44 +0700

    update server

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const router = require("./routes/lecturers");
+const routers = require("./routes/auth");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use(router);
+app.use(routers);
 
 //connect dengan port
 app.listen(port, () => {
