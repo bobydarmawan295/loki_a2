7cab4f8da1afeca772012ab08fa409f4dcc5b9bb
annisaulfa11 <2011522015_annisa@student.ac.id>
2022-04-29 14:51:23 +0700

    update curriculum_lo_details model

--- models/course_lo_details.js
+++ models/course_lo_details.js
@@ -1,36 +1,37 @@
 // const { Sequelize, DataTypes } = require('sequelize');
 
-const Sequelize = require('sequelize');
-const db= require('../database/conn');
+const Sequelize = require("sequelize");
+const db = require("../database/conn");
 
-const curriculum_lo_details = db.define('curriculum_lo_details', {
+const curriculum_lo_details = db.define(
+  "curriculum_lo_details",
+  {
     id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    curriculum_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false
+    curriculum_lo_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
     },
     course_lo_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false
+      type: Sequelize.INTEGER,
+      allowNull: false,
     },
     created_at: {
-        type: Sequelize.DATE,
+      type: Sequelize.DATE,
     },
     updated_at: {
-        type: Sequelize.DATE,
-    }
-},
+      type: Sequelize.DATE,
+    },
+  },
 
-{  
-
-    tableName: 'curriculum_lo_details',
-    timestamps: false  //Karena created_at dan update_at akan dibuat otomatis oleh sequelize
+  {
+    tableName: "curriculum_lo_details",
+    timestamps: false, //Karena created_at dan update_at akan dibuat otomatis oleh sequelize
     // freezeTableName: true
-    
-});
+  }
+);
 
 module.exports = curriculum_lo_details;
