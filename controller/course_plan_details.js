d6a1e968dff9fba263a9159de98736a3a2083612
annisaulfa11 <annisaulfasra@gmail.com>
2022-05-24 20:22:18 +0700

    update

--- controller/course_plan_details.js
+++ controller/course_plan_details.js
@@ -1,1 +1,55 @@
-console.log("ini controller");
+const CPD = require("../models/course_plan_details");
+const controller = {};
+
+controller.getCPDById = async (req, res) => {
+  try {
+    const cpd = await CPD.findAll({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(cpd[0]);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+controller.createCPD = async (req, res) => {
+  try {
+    await CPD.create(req.body);
+    res.json({
+      message: "Created",
+    });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+controller.updateCPD = async (req, res) => {
+  try {
+    await CPD.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json({
+      message: "Updated",
+    });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+controller.deleteCPD = async (req, res) => {
+  try {
+    await CPD.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json({
+      message: "Deleted",
+    });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+module.exports = controller;
