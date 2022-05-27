dfdd417081c7cfc67c7077ba8fe18bd06fa742a6
Boby Darmawan <2011522023@student.unand.ac.id>
2022-05-27 13:10:35 +0700

    add view and middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken');
+const users = require('../models/users');
+
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  // check json web token exists & is verified
+  if (token) {
+    jwt.verify(token, 'net ninja secret', (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.redirect('/login');
+      } else {
+        console.log(decodedToken);
+        next();
+      }
+    });
+  } else {
+    res.redirect('/login');
+  }
+};
+
+// check current user
+const checkUser = async (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, 'net ninja secret', async (err, decodedToken) => {
+      if (err) {
+        res.locals.user = null;
+        next();
+      } else {
+        let user = await users.findByPk(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+
+module.exports = { requireAuth, checkUser };
