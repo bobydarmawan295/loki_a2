09c9f6824ab6ff7902b7d8e3fd60fffd67e4994e
annisaulfa11 <annisaulfasra@gmail.com>
2022-06-18 10:35:27 +0700

    update course plan controller

--- controller/course_plan.js
+++ controller/course_plan.js
@@ -3,49 +3,45 @@ const courses = require("../models/courses");
 const lecturers = require("../models/lecturers");
 // const curricula = require("../models/curricula");
 
-
 const getCourses = async (req, res) => {
-    try {
-        await course_plans.findAll({
-            attributes:['id','name','code'],
-            include: [{
-                model: courses,
-                attributes:['name','semester','curriculum_id'],
-                required:true
+  try {
+    await course_plans
+      .findAll({
+        attributes: ["id", "name", "code"],
+        include: [
+          {
+            model: courses,
+            attributes: ["name", "semester", "curriculum_id"],
+            required: true,
+          },
+          {
+            model: lecturers,
+            attributes: ["id"],
+            through: {
+              attributes: ["updated_at", "created_at"],
             },
-            {
-                model: lecturers,
-                attributes:['id'],
-                through: {
-                    attributes: ["updated_at", "created_at"],
-                },
-                required:true
-            }
-            ],
-            where: {
-                id: req.params.id
-            }
-        })
-        .then((result) => {
-            if(result.length > 0){
-                // res.render('dosen/course_plan',{items: result})
-                res.status(200).json({
-                    message: 'mendapat data dosen',
-                    data: result
-                })
-            }else{
-                res.status(200).json({
-                    message: 'data tidak ada',
-                    data: []
-                })
-            }
-        })
+            required: true,
+          },
+        ],
+        where: {
+          id: req.params.id,
+        },
+      })
+      .then((result) => {
+        if (result.length > 0) {
+          res.render("dosen/course_plan", { items: result });
+        } else {
+          res.status(200).json({
+            message: "data tidak ada",
+            data: [],
+          });
+        }
+      });
+  } catch (error) {
+    res.status(404).json({
+      message: error,
+    });
+  }
+};
 
-    } catch (error) {
-        res.status(404).json({
-            message: error,
-        })
-    }  
-}
-
-module.exports = {getCourses};
+module.exports = { getCourses };
