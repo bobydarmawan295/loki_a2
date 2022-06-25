b98fa783144ef5d2a1197fd330f2267d8371937b
annisaulfa11 <annisaulfasra@gmail.com>
2022-06-25 22:37:41 +0700

    update

--- routes/dosen.js
+++ routes/dosen.js
@@ -2,6 +2,9 @@ const express = require("express");
 const { getAllCourses, createCourse } = require("../controller/courses");
 const { getCourses } = require("../controller/course_plan");
 const { getCourseLos, createCourseLos, updateCourseLos, deleteCourseLos, getCourseLosById } = require("../controller/course_los");
+const { getReferences, createReference, getReferenceById, updateReference, deleteReference } = require("../controller/course_plan_references");
+const { getDetail, getDetailById, createDetail, updateDetail, deleteDetail } = require("../controller/course_plan_details");
+const { getAssessments, getAssessmentsById, createAssessments, updateAssessments, deleteAssessments } = require("../controller/course_plan_assessments");
 
 const router = express.Router();
 
@@ -19,25 +22,18 @@ router.get("/dosen", (req, res) => {
   res.send("ini Halaman dosen");
 });
 
+//course
 router.get("/courses", getAllCourses);
-
 router.get("/coursesPlan/:id", getCourses);
-
-router.get("/coursesPlan/:id/CPMK", getCourseLos);
-
-// router.get('/courses', (req, res) => {
-//     res.render('courses');
-// });
-
-// router.get('/course-plan', (req, res) => {
-//     res.render('dosen/course_plan');
-// });
 
 router.get("/add-course", (req, res) => {
   res.render("dosen/add_rps");
 });
 
 router.post("/add-course", createCourse);
+
+//course plan
+router.get("/coursesPlan/:id/CPMK", getCourseLos);
 
 router.get("/coursesPlan/:id/add-cpmk", (req, res) => {
   res.render("dosen/add_cpmk");
@@ -48,5 +44,41 @@ router.get("/coursesPlan/:id/edit-cpmk/:id", getCourseLosById);
 router.put("/coursesPlan/:id/edit-cpmk/:id", updateCourseLos);
 
 router.delete("/coursesPlan/:id/CPMK/:id", deleteCourseLos);
+
+//reference
+router.get("/coursesPlan/:id/referensi", getReferences);
+router.get("/coursesPlan/:id/add-referensi", (req, res) => {
+  res.render("dosen/add_referensi");
+});
+router.post("/coursesPlan/:id/add-referensi", createReference);
+
+router.delete("/coursesPlan/:id/referensi/:id", deleteReference);
+
+router.get("/coursesPlan/:id/edit-referensi/:id", getReferenceById);
+router.put("/coursesPlan/:id/edit-referensi/:id", updateReference);
+
+//detail pertemuan
+router.get("/coursesPlan/:id/pertemuan", getDetail);
+router.get("/coursesPlan/:id/add-pertemuan", (req, res) => {
+  res.render("dosen/add_pertemuan");
+});
+router.post("/coursesPlan/:id/add-pertemuan", createDetail);
+
+router.delete("/coursesPlan/:id/pertemuan/:id", deleteDetail);
+
+router.get("/coursesPlan/:id/edit-pertemuan/:id", getDetailById);
+router.put("/coursesPlan/:id/edit-pertemuan/:id", updateDetail);
+
+//Penilaian
+router.get("/coursesPlan/:id/penilaian", getAssessments);
+router.get("/coursesPlan/:id/add-penilaian", (req, res) => {
+  res.render("dosen/add_penilaian");
+});
+router.post("/coursesPlan/:id/add-penilaian", createAssessments);
+
+router.delete("/coursesPlan/:id/penilaian/:id", deleteAssessments);
+
+router.get("/coursesPlan/:id/edit-penilaian/:id", getAssessmentsById);
+router.put("/coursesPlan/:id/edit-penilaian/:id", updateAssessments);
 
 module.exports = router;
